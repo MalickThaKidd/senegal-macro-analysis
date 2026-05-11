be9c66fb581e116b58ac9324150258675fda61ce
MalickThaKidd <xmvlvckx@gmail.com>
2026-05-11 11:55:55 +0000

    Add CAGR function and integrate GDP long-term growth analysis

--- src/indicators.py
+++ src/indicators.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# A. GDP Growth Rate Function
 def compute_growth_rate(df, column, new_column="growth_rate"):
     """
     Compute the percentage growth rate of a time series.
@@ -18,3 +19,27 @@ def compute_growth_rate(df, column, new_column="growth_rate"):
     df[new_column] = df[column].pct_change() * 100
     
     return df
+
+#B. CAGR function 
+def compute_cagr(df, column, start_year, end_year):
+    """
+    Compute the Compound Annual Growth Rate (CAGR) for a given time series.
+
+    Parameters:
+        df (pd.DataFrame): Input time-series DataFrame
+        column (str): Column used to compute CAGR
+        start_year (int): Starting year for CAGR calculation
+        end_year (int): Ending year for CAGR calculation
+    Returns:
+        float: CAGR value in percentage
+    """
+    df = df.copy()
+    df.index = pd.to_datetime(df.index)
+    df.sort_index(ascending=True, inplace=True)
+    start_value = df.loc[df.index.year == start_year, column].iloc[0]
+    end_value = df.loc[df.index.year == end_year, column].iloc[0]
+    years = end_year - start_year
+    cagr = ((end_value / start_value) ** (1 / years) - 1) * 100
+    
+    return cagr
+
